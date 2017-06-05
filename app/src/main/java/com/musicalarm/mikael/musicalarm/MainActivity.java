167cff91900548de7ee647694f804c1133cc60f0
Mikael Malmqvist <mikael.malmqvist.web@gmail.com>
2017-06-05 10:39:47 +0200

    add auth towards Spotify

--- app/src/main/java/com/musicalarm/mikael/musicalarm/MainActivity.java
+++ app/src/main/java/com/musicalarm/mikael/musicalarm/MainActivity.java
@@ -3,10 +3,26 @@ package com.musicalarm.mikael.musicalarm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+import com.spotify.sdk.android.player.ConnectionStateCallback;
+import com.spotify.sdk.android.player.Player;
+import com.spotify.sdk.android.player.PlayerNotificationCallback;
+import com.spotify.sdk.android.player.PlayerState;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ConnectionStateCallback, PlayerNotificationCallback {
+
+    private static final String CLIENT_ID = "22a32c3cb52747b0912c3701637d53db";
+    private static final String REDIRECT_URI = "musicalarm://callback";
+
+
+    protected static Player mPlayer;
+    private static final int REQUEST_CODE = 1337;
 
     private static List<AlarmItem> alarms = new ArrayList<>();
 
@@ -14,5 +30,55 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authSpotify();
+
+    }
+
+    // authorizes user towards Spotify
+    public void authSpotify() {
+        AuthenticationRequest.Builder builder = new AuthenticationRequest.Builder(
+                CLIENT_ID,
+                AuthenticationResponse.Type.TOKEN,
+                REDIRECT_URI);
+        builder.setScopes(new String[]{"user-read-private", "streaming"});
+        AuthenticationRequest request = builder.build();
+
+        AuthenticationClient.openLoginActivity(this, REQUEST_CODE, request);
+    }
+
+    @Override
+    public void onLoggedIn() {
+
+    }
+
+    @Override
+    public void onLoggedOut() {
+
+    }
+
+    @Override
+    public void onLoginFailed(Throwable throwable) {
+
+    }
+
+    @Override
+    public void onTemporaryError() {
+
+    }
+
+    @Override
+    public void onConnectionMessage(String s) {
+
+    }
+
+    @Override
+    public void onPlaybackEvent(EventType eventType, PlayerState playerState) {
+
+    }
+
+    @Override
+    public void onPlaybackError(ErrorType errorType, String s) {
+
     }
 }
